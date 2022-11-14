c97a226f0c7e9a8f708b0a68d18398e68cc22f5f
sahil11129 <112084296+sahil11129@users.noreply.github.com>
2022-11-14 20:54:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -185,16 +185,3 @@ resource null_resource setup_gitops_instance {
     }
   }
 }
-  
-  module "cp-watson-assistant" {
-  source = "github.com/cloud-native-toolkit/terraform-gitops-cp-watson-assistant.git"
-
-  gitops_config = module.gitops.gitops_config
-  git_credentials = module.gitops.git_credentials
-  server_name = module.gitops.server_name
-  namespace = module.gitops_namespace.name
-  kubeseal_cert = module.gitops.sealed_secrets_cert
-  operator_namespace= module.gitops_cp4d_operator.namespace
-  cpd_namespace = module.cp4d-instance.namespace
-}
-}
